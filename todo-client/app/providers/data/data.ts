fb9762af92b9230fd478f9c103e1e08fe7a76a21
Ralph Schaer <ralphschaer@gmail.com>
2016-07-04 20:45:17 +0200

    Polish

--- todo-client/app/providers/data/data.ts
+++ todo-client/app/providers/data/data.ts
@@ -1,20 +1,20 @@
 import {Storage, SqlStorage} from 'ionic-angular';
 import {Injectable} from '@angular/core';
- 
+
 @Injectable()
 export class Data {
- 
+
   private storage: Storage;
- 
-  constructor(){
-    this.storage = new Storage(SqlStorage, {name:'todo'});
+
+  constructor() {
+    this.storage = new Storage(SqlStorage, { name: 'todo' });
   }
- 
+
   getData() {
-    return this.storage.get('todos');  
+    return this.storage.get('todos');
   }
- 
-  save(data){
+
+  save(data) {
     let newData = JSON.stringify(data);
     this.storage.set('todos', newData);
   }
