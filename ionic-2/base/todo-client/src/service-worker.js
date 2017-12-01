7558d4e3aba5f7c298764d20e346a02d05b30f6a
Ralph Schaer <ralphschaer@gmail.com>
2017-12-01 12:50:13 +0100

    Upgrade libraries

--- ionic-2/base/todo-client/src/service-worker.js
+++ ionic-2/base/todo-client/src/service-worker.js
@@ -1,5 +1,5 @@
 /**
- * Check out https://googlechrome.github.io/sw-toolbox/docs/master/index.html for
+ * Check out https://googlechromelabs.github.io/sw-toolbox/docs/master/index.html for
  * more info on how to use sw-toolbox to custom configure your service worker.
  */
 
@@ -23,7 +23,7 @@ self.toolbox.precache(
 );
 
 // dynamically cache any other local assets
-self.toolbox.router.any('/*', self.toolbox.cacheFirst);
+self.toolbox.router.any('/*', self.toolbox.fastest);
 
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
